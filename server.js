836539062b9fca788889ea971c9ca52e2fc7fe37
Alw3ys <alw3ys@gmail.com>
2017-07-08 21:36:28 +0200

    port = 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 require('./routes')(app);
 
-var port = 80;
+var port = 3000;
 app.listen(port, function () {
     console.log('[%s INFO]: Starting Node server on *:%s', dateUtils.getCurrentFormattedTime(), port)
 });
